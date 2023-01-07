203d7a161db15aa3aeec6d847a07b0e136ede62e
Arpana <muriki.arpana@gmail.com>
2023-01-07 05:54:30 +0530

    Second commit

---
+++ src/main/java/stepDefinations/TC002_Sample.java
@@ -0,0 +1,10 @@
+package stepDefinations;
+
+public class TC002_Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git and Github");
+	}
+
+}
